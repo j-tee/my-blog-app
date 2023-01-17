b08f62beeafb1dd8e1b9d027b47b74b700523369
j-tee <juliustetteh@gmail.com>
2023-01-17 14:03:16 +0000

    Create data models

---
+++ db/migrate/20230117133951_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :author_id
+      t.string :title
+      t.string :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
