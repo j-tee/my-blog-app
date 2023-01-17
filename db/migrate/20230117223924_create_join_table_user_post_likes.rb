24285d88ea3cee0d6fcfa0d88ad08ecfe1d7dbc0
j-tee <juliustetteh@gmail.com>
2023-01-17 22:46:27 +0000

    Fix pending migration error

---
+++ db/migrate/20230117223924_create_join_table_user_post_likes.rb
@@ -0,0 +1,11 @@
+class CreateJoinTableUserPostLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.integer :author_id
+      t.integer :post_id
+      t.timestamps
+    end
+    add_index :likes, :author_id
+    add_index :likes, :post_id
+  end
+end
