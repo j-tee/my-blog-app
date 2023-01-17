aba5ed84b10fab2477afac148c56c03d642dbf81
j-tee <juliustetteh@gmail.com>
2023-01-17 23:07:52 +0000

    test-case

--- db/migrate/20230117134429_create_likes.rb
+++ db/migrate/20230117134429_create_likes.rb
@@ -1,8 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id
-      t.integer :post_id
       t.belongs_to :post, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
 
