e544f6cf8dabca8b9c1bb24f3a391a98f2c96ca2
j-tee <juliustetteh@gmail.com>
2023-01-19 12:24:24 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts, through: :comments
   has_many :posts, class_name: 'Post', foreign_key: 'user_id'
   has_many :likes, class_name: 'Like', foreign_key: 'user_id'
-  
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
