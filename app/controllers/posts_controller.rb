7df082a70e9aec4664e437c4e35a7e1530ff2383
Tetteh Julius <juliustetteh@gmail.com>
2023-01-25 19:25:07 +0000

    Add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @place_holder = 'List of users'
+    @posts = Post.user_posts(params[:user_id]).paginate(page: params[:page], per_page: 10)
   end
 
   def show
-    @place_holder = 'List of users'
+    @post = Post.user_post(params[:user_id], params[:post_id])
   end
 end
--- app/javascript/show_more.js
+++ app/javascript/show_more.js
@@ -1,19 +1,20 @@
 let postList = document.querySelectorAll('li:nth-child(n+4)');
-let button = document.querySelector('#show-more-button');
+let button = document.getElementById('show-more-button');
 console.log(button)
 
 button.addEventListener('click', function ()
 {
-    for (let i = 0; i < postList.length; i++)
+    for (let i = 3; i < postList.length; i++)
     {
-        if (postList[i].style.display === 'none')
+        console.log(postList[i].style.display)
+        if (postList[i].style.display === 'none' || postList[i].style.display === '')
         {
             postList[i].style.display = 'flex';
-            document.getElementById("show-more-button").innerText = "SHow Less";
+            document.getElementById("show-more-button").innerText = "Show Less";
         } else
         {
             postList[i].style.display = 'none';
-            document.getElementById("show-more-button").innerText = "SHow More";
+            document.getElementById("show-more-button").innerText = "Show More";
         }
 
     }
