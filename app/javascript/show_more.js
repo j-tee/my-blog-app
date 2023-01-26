7315b3cb9e908e577644e01228f323387afc7af3
j-tee <juliustetteh@gmail.com>
2023-01-26 09:05:09 +0000

    Fix Show All Posts button link to all posts page

--- app/javascript/show_more.js
+++ app/javascript/show_more.js
@@ -1,21 +1,25 @@
-let postList = document.querySelectorAll('li:nth-child(n+4)');
-let button = document.getElementById('show-more-button');
-console.log(button)
-
-button.addEventListener('click', function ()
-{
-    for (let i = 3; i < postList.length; i++)
+window.onload = function() {
+    let postList = document.querySelectorAll('li:nth-child(n+4)');
+    let button = document.getElementById('show-more-button');
+    console.log(button)
+    
+    button.addEventListener('click', function ()
     {
-        console.log(postList[i].style.display)
-        if (postList[i].style.display === 'none' || postList[i].style.display === '')
+        for (let i = 0; i < postList.length; i++)
         {
-            postList[i].style.display = 'flex';
-            document.getElementById("show-more-button").innerText = "Show Less";
-        } else
-        {
-            postList[i].style.display = 'none';
-            document.getElementById("show-more-button").innerText = "Show More";
+            console.log(postList[i].style.display)
+            if (postList[i].style.display === 'none' || postList[i].style.display === '')
+            {
+                postList[i].style.display = 'flex';
+                document.getElementById("show-more-button").innerText = "Show Less";
+            } else
+            {
+                postList[i].style.display = 'none';
+                document.getElementById("show-more-button").innerText = "Show More";
+            }
+    
         }
+    });
+}
 
-    }
-});
+
