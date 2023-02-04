72a7780d87a7730c80245703f3782654e51f5ffa
j-tee <juliustetteh@gmail.com>
2023-02-04 08:56:53 +0000

    Update ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
   def initialize(member)
     can :delete, Post do |post|
-      post.user.member_id == member.id || member.admin?
+      post&.user&.member_id == member&.id || member&.try(:admin?)
     end
     can :delete, Comment do |comment|
-      comment.user.member_id == member.id || member.admin?
+      comment&.user&.member_id == member&.id || member&.try(:admin?)
     end
   end
 end
